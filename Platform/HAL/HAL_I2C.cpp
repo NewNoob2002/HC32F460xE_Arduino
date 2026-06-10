fa2b5458c95234ad75a50d57216d821b949cdf20
NewNoob2002 <guptianci@gmail.com>
2026-06-10 13:58:06 +0800

    build: 更新CMake构建系统并重构I2C配置
    1. 移除旧的arm-none-eabi.cmake文件，替换为新的CMake工具链配置
    2. 更新CMakeLists.txt，调整项目名称和版本信息
    3. 重构I2C相关头文件，修改I2C单元和设备地址定义
    4. 添加新的I2C缓冲区大小定义，优化I2C初始化和更新函数
    5. 更新CMakePresets.json，配置GCC调试和发布构建模式
    6. 移除冗余的proj_hc32.code-workspace文件
    7. 整理Platform目录结构，添加Config子目录以管理配置文件

--- Platform/HAL/HAL_I2C.cpp
+++ Platform/HAL/HAL_I2C.cpp
@@ -3,11 +3,12 @@
 #include "bq40z50.h"
 #include "mp2762a.h"
 
-void i2c_scan_callback(void *e)
-{
-    if (e == nullptr)
+void
+i2c_scan_callback(void* e) {
+    if (e == nullptr) {
         return;
-    uint8_t addr = *(uint8_t *)e;
+    }
+    uint8_t addr = *(uint8_t*)e;
     switch (addr) {
         default: {
             CORE_DEBUG_PRINTF("0x%02X - Unknow", addr);
@@ -17,28 +18,30 @@ void i2c_scan_callback(void *e)
         case 0x0B: {
             CORE_DEBUG_PRINTF("0x%02X - BQ40Z50 Battery Pack Manager / Fuel gauge", addr);
             systemInfo.online_device.bq40z50 = 1;
-						bq40z50Begin(&Wire);
-						checkBatteryInfo(&systemInfo.powerMonitor.batteryInfo);
+            bq40z50Begin(&Wire);
+            checkBatteryInfo(&systemInfo.powerMonitor.batteryInfo);
             break;
         }
 
         case 0x5C: {
             CORE_DEBUG_PRINTF("0x%02X - MP27692A Power Management / Charger", addr);
             systemInfo.online_device.mp2762 = 1;
-						mp2762aBegin(&Wire);
-						ChagrerMp2762_begin(&systemInfo.powerMonitor.batteryInfo);
+            mp2762aBegin(&Wire);
+            ChagrerMp2762_begin(&systemInfo.powerMonitor.batteryInfo);
+            strcpy(systemInfo.hardware_version, "V1.5");
             break;
         }
     }
 }
 
-int HAL::I2C_Scan()
-{
+int
+HAL::I2C_Scan() {
     if (!Wire.begin()) {
         CORE_DEBUG_PRINTF("I2C: init failed");
         return -1;
     }
     CORE_DEBUG_PRINTF("I2C: device scanning...");
-
+    strcpy(systemInfo.software_version, SoftwareVersion);
+    strcpy(systemInfo.hardware_version, "V1.3");
     return Wire.scanDeivces(i2c_scan_callback);
 }
